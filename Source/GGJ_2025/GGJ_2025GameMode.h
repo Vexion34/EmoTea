b028ce15fac627b93bd6b17f263780f1d4784016
Vexion34 <aure34620@gmail.com>
2025-01-22 13:55:05 +0100

    Initial commit

---
+++ Source/GGJ_2025/GGJ_2025GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGJ_2025GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGGJ_2025GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGGJ_2025GameMode();
+};
+
+
+
